ff615e73c7e2bebdcb0165611623ed9b609272ef
Elevila Thompson <ele.thompson12@gmail.com>
2017-03-14 18:02:50 -0700

    Started JDBCTutorial

---
+++ JDBCTutorial/src/main/java/edu/cs313/byui/jdbctutorial/Persons.java
@@ -0,0 +1,104 @@
+package edu.cs313.byui.jdbctutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 0249239
+ */
+@WebServlet(name = "Persons", urlPatterns = {"/Persons"})
+public class Persons extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try {
+            // Load JDBC driver classes
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Persons.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+                // Create connection getConnection(server location, username, password)
+                Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ancestors", "postgres", "postgres");    
+                // Start a prepared statement
+                Statement statement = connection.createStatement();
+                // Results from the query
+                ResultSet resultSet = statement.executeQuery("SELECT id, name FROM person");
+            
+            // Print out to the page
+            response.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet Persons</title>");            
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>JDBC Tutorial - Persons</h1>");
+                
+                while(resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    
+                    out.println("<p>"+ id + ", " + name + "</p>");
+                }
+                
+                out.println("</body>");
+                out.println("</html>");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Persons.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
